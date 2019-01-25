9ed8353e73e9d2e30028c65d3d38d080cec65be4
Sergo <rogaldh@radsh.red>
2019-01-26 00:08:01 +0300

    Fix test scripts

--- test/babel-register.js
+++ test/babel-register.js
@@ -3,5 +3,4 @@ const babelrc = require('../.babelrc.json')
 // eslint-disable-next-line node/no-unpublished-require
 require('@babel/register')({
   ...babelrc.env.cjs,
-  only: [/node_modules/, /test/, /src/],
 })
