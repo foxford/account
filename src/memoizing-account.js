dc788035c9ad8a0a292bc918a25dc5a6c1e877ab
Sergo <rogaldh@radsh.red>
2019-03-21 13:24:10 +0300

    Allow to memoize pending promises (#19)

---
+++ src/memoizing-account.js
@@ -0,0 +1,47 @@
+import { Account } from './account'
+
+export class MemoizingAccount extends Account {
+  constructor (...argv) {
+    super(...argv)
+
+    this.pendingRequests = new Map([])
+  }
+
+  memoizedPromise (label, fn) {
+    const store = this.pendingRequests
+    const maybePending = store.get(label)
+
+    let nextPromise
+
+    if (!maybePending) {
+      nextPromise = fn().then((data) => {
+        store.delete(label)
+
+        return data
+      })
+      store.set(label, nextPromise)
+
+      return nextPromise
+    }
+
+    return maybePending
+  }
+
+  account (storageLabel = '') {
+    const label = storageLabel || this.id
+
+    return this.memoizedPromise(`account_${label}`, () => super.account(storageLabel))
+  }
+
+  tokenData (storageLabel) {
+    const label = storageLabel || this.id
+
+    return this.memoizedPromise(`tokendata_${label}`, () => super.tokenData(storageLabel))
+  }
+
+  revokeRefreshToken (storageLabel) {
+    const label = storageLabel || this.id
+
+    return this.memoizedPromise(`revoketoken_${label}`, () => super.revokeRefreshToken(storageLabel))
+  }
+}
